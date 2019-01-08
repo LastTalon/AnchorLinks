89aa57ff95b5196f729e88edc2f4405f14ad5647
Lucas Gangstad <last_talon@new.rr.com>
2019-01-08 07:55:07 -0600

    Update equality to identity operators

--- anchor.js
+++ anchor.js
@@ -3,7 +3,7 @@ chrome.storage.sync.get("active", function(data) {
 		let anchors = document.getElementsByTagName("*");
 		for (let i = 0; i < anchors.length; i++) {
 			let display = window.getComputedStyle(anchors[i]).display;
-			if (anchors[i].id != "" && (display == "inline" || display == "inline-block")) {
+			if (anchors[i].id !== "" && (display === "inline" || display === "inline-block")) {
 				let link = document.createElement("A");
 				let space = document.createElement("DIV");
 				space.className = "anchor-space";
